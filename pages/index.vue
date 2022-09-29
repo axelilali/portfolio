a9794f4100ed9519708d6d11a1cb19626a549c11
Axel Ilali <axel.ilali@yahoo.fr>
2022-09-30 00:58:20 +0100

    feat: add github link

--- pages/index.vue
+++ pages/index.vue
@@ -13,16 +13,18 @@
               My name is Axel Ilali and I enjoy creating things that live on the
               internet.
             </p>
-            <span
-              data-aos="fade-up"
-              data-aos-delay="300"
-            >Find me on
+            <span data-aos="fade-up" data-aos-delay="300">Find me on
               <strong><a
                 tabindex="blank"
                 rel="noopener noreferrer"
                 href="https://www.linkedin.com/in/axel-ilali/"
+                target="blank"
               >LinkedIn</a></strong>,
-              <strong><a href="" rel="noopener noreferrer">Github</a></strong>
+              <strong><a
+                href="https://github.com/axelilali"
+                rel="noopener noreferrer"
+                target="blank"
+              >Github</a></strong>
               and
               <strong><a
                 target="blank"
@@ -30,12 +32,8 @@
                 href="https://twitter.com/im__axel"
               >Twitter</a></strong>
             </span>
-            <a
-              data-aos="fade-up"
-              data-aos-delay="400"
-              class="button"
-              href="mailto:ilali.axel@gmail.com"
-            >Let's work together</a>
+            <a data-aos="fade-up" data-aos-delay="400" class="button" href="mailto:ilali.axel@gmail.com">Let's work
+              together</a>
           </b-col>
         </b-row>
       </b-container>
@@ -57,11 +55,7 @@
               <h2>About Me</h2>
               <h3>
                 Mbote
-                <img
-                  height="20"
-                  src="~assets/waving-hand.png"
-                  alt="Waving hand"
-                >
+                <img height="20" src="~assets/waving-hand.png" alt="Waving hand">
                 ! My name is Axel and I enjoy creating things that live on the
                 internet. My interest in web development started back in 2012
                 when I decided to try HTML & CSS.
@@ -83,12 +77,7 @@
       <section id="work">
         <div v-for="(client, key) in clients" :key="key" class="client-box">
           <div class="name">
-            <img
-              v-if="client.logo"
-              width="150"
-              :src="require('../assets/clients/' + client.logo)"
-              :alt="client.name"
-            >
+            <img v-if="client.logo" width="150" :src="require('../assets/clients/' + client.logo)" :alt="client.name">
             <p v-else>
               {{ client.name }}
             </p>
@@ -107,12 +96,7 @@
         <b-container>
           <b-row>
             <b-col>
-              <img
-                data-aos="zoom-in"
-                width="300"
-                src="~assets/me-3.png"
-                alt="Axel Memoji"
-              >
+              <img data-aos="zoom-in" width="300" src="~assets/me-3.png" alt="Axel Memoji">
               <h2>What's next ?</h2>
               <p>
                 Feel free to shoot me an email to talk about your next big
@@ -223,6 +207,7 @@ h3,
 p {
   font-weight: lighter;
 }
+
 #cursor {
   display: none;
   position: fixed;
@@ -234,11 +219,13 @@ p {
   transform: translate(-50%, -50%);
   transition: all ease-out 0.1s;
   pointer-events: none;
+
   &.grow {
     width: 80px;
     height: 80px;
   }
 }
+
 .button {
   color: $primary;
   border: 2px solid $primary;
@@ -247,16 +234,19 @@ p {
   padding: 15px 30px;
   border-radius: 40px;
   transition: all ease 0.2s;
+
   &:hover {
     background: $primary;
     color: $secondary;
     text-decoration: none;
+
     #cursor {
       width: 100px;
       height: 100px;
     }
   }
 }
+
 #intro {
   position: fixed;
   width: 50%;
@@ -266,12 +256,14 @@ p {
   color: $primary;
   display: flex;
   align-items: center;
+
   span {
     color: #b5b6bd;
     font-size: 0.8125rem;
     margin-bottom: 50px;
     display: block;
     font-weight: lighter;
+
     a {
       color: #fff;
     }
@@ -282,9 +274,11 @@ p {
     margin-bottom: 50px;
   }
 }
+
 main {
   width: 50%;
   margin-left: 50%;
+
   section {
     height: 100vh;
     position: sticky;
@@ -293,26 +287,32 @@ main {
     align-items: center;
     padding: 50px;
   }
+
   #memoji {
     z-index: 1;
     text-align: center;
     background: url("~assets/bg-2.jpg") no-repeat;
     background-size: cover;
+
     img {
       max-width: 100%;
     }
   }
+
   #about {
     z-index: 2;
     background: #fff;
     color: $secondary;
+
     h2,
     h3 {
       margin-bottom: 20px;
     }
+
     p {
       color: $alternate;
     }
+
     span {
       display: inline-block;
       padding: 2px 10px;
@@ -324,6 +324,7 @@ main {
       border-radius: 20px;
     }
   }
+
   #work {
     z-index: 3;
     display: grid;
@@ -331,18 +332,22 @@ main {
     grid-gap: 2px;
     padding: 0px;
     background: #fff;
+
     img {
       margin: 0 auto;
     }
+
     .client-box {
       position: relative;
       display: flex;
       align-items: center;
       justify-content: center;
       height: 100%;
+
       p {
         margin: 0;
       }
+
       .name,
       .desc {
         position: absolute;
@@ -354,34 +359,41 @@ main {
         align-items: center;
         padding: 30px;
       }
+
       .name {
         background: $alternate-2;
         z-index: 3;
         transition: all ease 0.1s;
         font-weight: bold;
         justify-content: center;
+
         p {
           font-size: 1.25rem;
         }
       }
+
       .desc {
         background: #fff;
         z-index: 2;
+
         p {
           transition: all ease 0.3s;
           transform: translateY(10px);
         }
       }
+
       &:hover {
         .name {
           opacity: 0;
         }
+
         .desc p {
           transform: translateY(0px);
         }
       }
     }
   }
+
   #contact {
     z-index: 4;
     background: $primary;
@@ -396,6 +408,7 @@ main {
     }
   }
 }
+
 @media screen and (max-width: 768px) {
   #intro {
     position: relative;
@@ -403,30 +416,37 @@ main {
     padding: 20px;
     text-align: center;
   }
+
   main {
     width: 100%;
     margin-left: 0;
+
     #memoji {
       background-position: center;
     }
+
     #about {
       padding: 0 20px;
     }
   }
 }
+
 @media screen and (max-width: 576px) {
   #cursor {
     display: none;
   }
+
   main {
     #work {
       img {
         max-width: 100%;
       }
+
       .client-box .desc p {
         font-size: 0.875rem;
       }
     }
+
     #contact {
       img {
         max-width: 100%;
@@ -439,20 +459,25 @@ main {
   h1 {
     font-size: 30px;
   }
+
   main {
     #about {
       position: relative;
       height: max-content;
+
       .container {
         padding: 50px 10px;
       }
+
       h3 {
         font-size: 1.5rem;
       }
     }
+
     #work {
       .client-box .desc {
         padding: 20px;
+
         p {
           font-size: 0.625rem;
         }
